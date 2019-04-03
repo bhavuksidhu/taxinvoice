7edd686e598c7d66ba518a37e41ca6062761bc7e
Mohd Sameer <mohdsameer1310@gmail.com>
2019-04-03 15:15:47 +0530

    JS issue solved

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,9 +3,6 @@ class Invoice < ApplicationRecord
 	accepts_nested_attributes_for :goods_details, reject_if: :all_blank, allow_destroy: true
 	
 	def serial_number
-		# "2019/20/%.4d" % id
-		# DateTime.now.year/DateTime.now.year % 100 + 1/"%.4d" % id
-		binding.pry
 		month = Date.today.month
 		a =  Invoice.where("created_at < ? AND created_at > ?", "31 Mar #{Date.today.year}", "1 Apr #{Date.today.year+1}").count
 		if month = 1 || 2 || 3
