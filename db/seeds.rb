1a9b8232cde2f022217f27e758af71c943784dfc
Mohd Sameer <mohdsameer1310@gmail.com>
2019-04-06 22:46:29 +0530

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create( email: "arshadmalikcool@gmail.com", password: "taxinvoice@123")
+User.create( email: "parveshvats322@yahoo.com", password: "taxinvoice@123")
+User.create( email: "bhavuksidhu@gmail.com", password: "taxinvoice@123")
