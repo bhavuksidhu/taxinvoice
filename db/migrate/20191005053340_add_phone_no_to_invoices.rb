682d90aa8ec4511318d0c3ab6a5498e89c563089
bhavuk <you@example.com>
2019-10-05 14:26:31 +0530

    changes

--- db/migrate/20191005053340_add_phone_no_to_invoices.rb
+++ db/migrate/20191005053340_add_phone_no_to_invoices.rb
@@ -1,5 +1,5 @@
 class AddPhoneNoToInvoices < ActiveRecord::Migration[5.1]
   def change
-    add_column :invoices, :phone_no, :integer, limit: 10
+    add_column :invoices, :phone_no, :integer
   end
 end
